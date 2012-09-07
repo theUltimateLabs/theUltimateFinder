be0cac323684fc0efe973448462e2937fc94b629
RobB <rob@theultimatelabs.com>
2012-09-07 07:40:27 -0600

    example

--- Finder/src/com/theultimatelabs/finder/MainActivity.java
+++ Finder/src/com/theultimatelabs/finder/MainActivity.java
@@ -2,16 +2,29 @@ package com.theultimatelabs.finder;
 
 import android.app.*;
 import android.os.*;
-import android.view.*;
-import android.widget.*;
+import android.util.*;
+import com.google.android.gcm.*;
 
 public class MainActivity extends Activity
 {
+ 	public final static String TAG = "Main";
+	public final static String SENDER_ID = "762418178161";
     /** Called when the activity is first created. */
-    @Override
+
+
+	@Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		GCMRegistrar.checkDevice(this);
+		GCMRegistrar.checkManifest(this);
+		final String regId = GCMRegistrar.getRegistrationId(this);
+		if (regId.equals("")) {
+			GCMRegistrar.register(this,SENDER_ID);
+		} else {
+			Log.v(TAG, "Already registered");
+		}
     }
 }
